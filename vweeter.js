72d271f84921afe34c3377898efea9928a279caa
Ghost <Ghost@Ghosts-Mac.local>
2017-05-18 22:53:43 +0800

    v2.0 b27

--- vweeter.js
+++ vweeter.js
@@ -18,7 +18,7 @@ var timer = {};
 
 Vweeter = () => {
 
-    console.log(process.S3_KEY);
+    console.log('S3_KEY: ' ,process.S3_KEY);
 
     AWS.config.update({
         accessKeyId: process.env.S3_KEY,
